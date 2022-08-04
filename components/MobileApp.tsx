94316da37fd1ce75c343859fd3ae1c1d049a357e
Eric Echemane <eechemane29@gmail.com>
2022-08-05 00:31:36 +0800

    video hosted from cloudinary

--- components/MobileApp.tsx
+++ components/MobileApp.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function MobileApp() {
     return (
         <div className='mobile'>
-            <video src="/app_demo.mp4" muted autoPlay loop></video>
+            <video src="https://res.cloudinary.com/dmwjwtg1g/video/upload/v1659630588/samples/app_demo_a6evsn.mp4" muted autoPlay loop></video>
             <img className='frame' src="/android_frame.png" alt="mobile app frame" />
         </div>
     );
