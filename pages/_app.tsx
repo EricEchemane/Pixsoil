61f2ad5822c0d79779559eb51d4af0e7d06f4771
Eric Echemane <eechemane29@gmail.com>
2022-08-04 21:59:40 +0800

    new body background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { RouterTransition } from 'components/RouterTransition';
