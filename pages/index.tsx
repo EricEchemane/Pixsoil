9215ed009c1523d400c9c3d1a935283d02486259
Eric Echemane <eechemane29@gmail.com>
2022-08-04 17:00:16 +0800

    added download button and link to /web

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+import { Button, Group, Stack, Text } from '@mantine/core';
+import { NextLink } from '@mantine/next';
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import { IconArrowNarrowRight, IconDownload } from '@tabler/icons';
 
 const Home: NextPage = () => {
   return (
-    <h1>pixsoil</h1>
+    <>
+      <Head>
+        <title> Pixsoil - Soil Type Scanner </title>
+      </Head>
+
+      <Stack align={'center'} spacing={0}>
+        <img src='/logo.png' alt='logo' width={'250px'} />
+        <Text size={'xl'} > Scan your soil for better planting  </Text>
+        <Text color={'dimmed'} >
+          A machine learning web and mobile application soil type classification for plant decision making
+        </Text>
+
+        <Group position='center' mt='5rem'>
+          <Button href={'/app/Pixsoil.apk'} download component={NextLink} size='lg' style={{ backgroundColor: '#a68c67' }}>
+            Download Android App &nbsp; <IconDownload />
+          </Button>
+          <Button href={'/web'} component={NextLink} size='lg' style={{ backgroundColor: '#a68c67' }}>
+            Try Pixsoil for Web &nbsp; <IconArrowNarrowRight />
+          </Button>
+        </Group>
+      </Stack>
+    </>
   );
 };
 
