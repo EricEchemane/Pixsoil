b90936ffcda461a2cca14b61e6c293d89508ac0f
Eric Echemane <eechemane29@gmail.com>
2022-08-04 12:19:02 +0800

    new favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="logo.png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
