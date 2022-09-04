b4ce9ceadffce42cae454e9cb8f638d9f09614f6
Eric Echemane <eechemane29@gmail.com>
2022-09-04 11:21:58 +0800

    added poster to mobile app frame

--- components/MobileApp.tsx
+++ components/MobileApp.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
 
 export default function MobileApp() {
     return (
         <div className='mobile'>
-            <video src="https://res.cloudinary.com/dmwjwtg1g/video/upload/v1659630588/samples/app_demo_a6evsn.mp4" muted autoPlay loop></video>
+            <video
+                poster='/mobile_app_poster.png'
+                src="https://res.cloudinary.com/dmwjwtg1g/video/upload/v1659630588/samples/app_demo_a6evsn.mp4"
+                muted
+                autoPlay
+                loop />
             <img className='frame' src="/android_frame.png" alt="mobile app frame" />
         </div>
     );
