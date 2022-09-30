0faa77eb2b7d16bc42c08255915d9428e16c520e
Eric Echemane <eechemane29@gmail.com>
2022-09-30 10:03:36 +0800

    added suitable plants dictionary and fixed casing

---
+++ model/plants.ts
@@ -0,0 +1,145 @@
+export const plants = {
+    "Majority of vegetables": [
+        {
+            "name": "Broccoli",
+            "image":
+                "https://solidstarts.com/wp-content/uploads/Broccoli_edited-480x320.webp",
+            "description":
+                "Broccoli is moisture-loving plant so never let the soil entirely dry out. If you are careful at harvest time to preserve as many leaves as possible, stems and small heads will grow that produce a second harvest.",
+        },
+        {
+            "name": "Potato",
+            "image": "https://c.ndtvimg.com/p5qg74v8_potato_625x300_01_August_18.jpg",
+            "description":
+                "There are so many more types of potatoes than we will ever find in stores. Start with certified, disease-free seed potatoes to give yours a good start.",
+        },
+        {
+            "name": "Daikon radish",
+            "image":
+                "https://t1.uc.ltmcdn.com/en/posts/0/5/9/serve_raw_11950_1_600.jpg",
+            "description":
+                "Daikon is a winter radish that takes approximately 70 days to mature. Look for white, purple, red, and green varieties.",
+        },
+        {
+            "name": "Brussels sprouts",
+            "image":
+                "https://solidstarts.com/wp-content/uploads/Brussels-Sprouts-480x320.webp",
+            "description":
+                "Get extra value from your Brussels sprouts harvest by cleaning and cooking the leaves as well as the sprouts.",
+        },
+        {
+            "name": "Cabbage (red and green)",
+            "image":
+                "https://myconsciouseating.com/wp-content/uploads/2020/11/cabbage-substitute.jpg",
+            "description":
+                "Green cabbage is traditionally used for cole-slaw and soups. Red Express is a highly recommended red variety known for its vitamin C and A content.",
+        },
+        {
+            "name": "Cabbage (Napa and savoy)",
+            "image":
+                "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/24172455/ING-savoy-cabbage-main.jpg",
+            "description":
+                "Napa cabbage also goes by the name Chinese cabbage. Savoy is a cabbage with leaves somewhat similar to romaine lettuce.",
+        },
+        {
+            "name": "Cauliflower",
+            "image":
+                "https://www.seedsfoodmarket.ca/wareHouse/wp-content/uploads/cauliflower_commodity-page.png",
+            "description":
+                "Cauliflower does best in nutrient-rich soil so be sure to add compost or another fertilizer like fish emulsion on a regular basis.",
+        },
+        {
+            "name": "Kale",
+            "image":
+                "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F05%2F26%2F305-Easy-Garlic-Kale-ddmfs-4x3-3910.jpg&q=60",
+            "description":
+                "Kale is another cool weather vegetable that sweetens with a touch of frost.",
+        },
+        {
+            "name": "Bean (Bush or Pole)",
+            "image":
+                "https://www.almanac.com/sites/default/files/styles/large/public/image_nodes/green_beans_brytta_gettyimages.jpg?itok=98_LlmqZ",
+            "description":
+                "Pick beans when they are full-size and ripe but do not let them get so big that they look about to burst. Harvest carefully to allow the plant to continue producing more beans.",
+        },
+        {
+            "name": "Pea",
+            "image":
+                "https://s3-us-west-2.amazonaws.com/melingoimages/Images/70262.jpg",
+            "description":
+                "Snap and snow peas do best with trellis even if they say they are no-trellis varieties. Pea pods can be harvested at any time: the whole thing is delicious.",
+        },
+    ],
+    "Bulbs and Tubers": [
+        {
+            "name": "Onion",
+            "image":
+                "https://static.india.com/wp-content/uploads/2020/08/red-onion.jpg",
+            "description":
+                "What is the benefit of onions? Onions contain organic sulfur compounds. These compounds are the reason why onions have such a sharp, strong taste and smell. Organic sulfur compounds help reduce the level of cholesterol in your body and may also help break down blood clots, lowering your risk for heart disease and stroke.",
+        },
+        {
+            "name": "Garlic",
+            "image":
+                "https://organic-village.co.th/wp-content/uploads/2021/11/1-Garlic.jpeg",
+            "description":
+                "Garlic helps Boost Your Body's Immune System. reduce High Blood Pressure, reduce Cholesterol Levels, helps with prevention of cancer, has antibiotic properties and can improve athletic performance.",
+        },
+        {
+            "name": "Allium",
+            "image":
+                "https://www.gardendesign.com/pictures/images/675x529Max/site_3/serendipity-allium-allium-proven-winners_16388.jpg",
+            "description":
+                "The economically most important Allium crop species (common onion and garlic) are worldwide used as spices, vegetables, and medicinal plants. Traditionally, they play a very important role in the daily diet also in Asia. Here they can be seen under cultivation in every home garden.",
+        },
+        {
+            "name": "Daffodil",
+            "image":
+                "https://www.proflowers.com/blog/wp-content/uploads/2021/04/daffodil-care-guide-thumb.jpg",
+            "description":
+                "Daffodils are said to symbolize friendship but also new beginnings. They are one of the first signs of Spring and are also strongly associated with Easter because of this reason. In China, the daffodil symbolizes good fortune and they are the official Chinese New Year symbol.",
+        },
+        {
+            "name": "Tulip",
+            "image":
+                "https://cdnprod.mafretailproxy.com/sys-master-root/ha5/h82/9957063589918/1660055_main.jpg_480Wx480H",
+            "description":
+                "Tulip bulbs are a famine food, and they must be prepared correctly, that is the centers must be removed. Fortunately tulip petals are more edible. The petals can be eaten raw or cooked but loose much of their color when cooked. They can have many flavors: Bland, beans, peas, and cucumbers.",
+        },
+        {
+            "name": "Beets",
+            "image":
+                "https://post.healthline.com/wp-content/uploads/2020/09/AN172-Beets-732x549-Thumb_0.jpg",
+            "description":
+                "Beets are rich in folate (vitamin B9) which helps cells grow and function. Folate plays a key role in controlling damage to blood vessels, which can reduce the risk of heart disease and stroke. Beets are naturally high in nitrates, which are turned into nitric oxide in the body.",
+        },
+        {
+            "name": "Carrots",
+            "image":
+                "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/4/25/3/shutterstock_440493100_5-second-Studio_carrots.jpg.rend.hgtvcom.1280.853.suffix/1524688181811.jpeg",
+            "description":
+                "Carrot seeds can be sown about 2 to 3 weeks before the last spring frost date. Find your local frost dates here. Ideally, you want the soil to have both dried out and warmed up a little after the winter. Seeds germinate after the soil temperature is at least 40° and germinate best at 55-65°, not exceeding 75°F.",
+        },
+        {
+            "name": "Parsnips",
+            "image":
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_963ccy7iYAERRXtDZF3ZvfLfwwf60Z9zNeYB03GECsdk-sDNgyniR_OT1Hpu5Dfdx0g&usqp=CAU",
+            "description":
+                "It resembles a carrot in this way, and indeed they are part of the same family. But parsnips don't taste like carrots. They're sweeter—think sweet potatoes—and they have a delicious naturally nutty or earthy flavor. Parsnips are typically available in the fall and winter.",
+        },
+        {
+            "name": "Turnips",
+            "image":
+                "https://cdn-prod.medicalnewstoday.com/content/images/articles/284/284815/a-bunch-of-turnips-on-a-table.jpg",
+            "description":
+                "Turnips are a cruciferous vegetable with multiple health benefits. They boast an impressive nutritional profile, and their bioactive compounds, such as glucosinolates, may support blood sugar control, protect against harmful bacteria, and provide anticancer and anti-inflammatory effects.",
+        },
+        {
+            "name": "Radishes",
+            "image":
+                "https://www.tasteofhome.com/wp-content/uploads/2019/05/radish-shutterstock_1010435131.jpg?fit=700,700",
+            "description":
+                "Radishes are rich in antioxidants and minerals like calcium and potassium. Together, these nutrients help lower high blood pressure and reduce your risks for heart disease. The radish is also a good source of natural nitrates that improve blood flow.",
+        },
+    ],
+};
--- model/soil_properties.ts
+++ model/soil_properties.ts
@@ -1,72 +1,72 @@
 export const soil_properties = [
     {
         "name": "clay",
-        "colors": "red, orange, brown",
-        "grain_size": "small and fine",
-        "acidity": "alkaline",
-        "texture": "sticky and lumpy when wet. hard with cracks when dry",
-        "how_to_test": "can form a ball when you squeeze a handful",
-        "water_and_nutrient_holding_capacity": "high",
-        "porosity": "low",
-        "other_properties":
+        "Colors": "red, orange, brown",
+        "Grain Size": "small and fine",
+        "Acidity": "alkaline",
+        "Texture": "sticky and lumpy when wet. hard with cracks when dry",
+        "How to verify the type": "can form a ball when you squeeze a handful",
+        "Water and Nutrient Holding Capacity": "high",
+        "Porosity": "low",
+        "Other properties":
             "can hold water and nutrients longer than silty and sandy soil. Drains slower",
-        "how_to_improve": "add organic matters that are high porosity. cultivation",
-        "suitable_plants": "majority of vegetables"
+        "How to improve the soil": "add organic matters that are high porosity. cultivation",
+        "Suitable Plants": "majority of vegetables"
     },
     {
         "name": "sand",
-        "colors": "light brown, gray",
-        "grain_size": "big and course",
-        "acidity": "acidic",
-        "texture": "gritty and rough",
-        "how_to_test":
+        "Colors": "light brown, gray",
+        "Grain Size": "big and course",
+        "Acidity": "acidic",
+        "Texture": "gritty and rough",
+        "How to verify the type":
             "grab a handful and open your hand, particles fall off through your fingers",
-        "water_and_nutrient_holding_capacity": "low",
-        "porosity": "high",
-        "other_properties":
+        "Water and Nutrient Holding Capacity": "low",
+        "Porosity": "high",
+        "Other properties":
             "easy to cultivate. Drains quickly. Water and nutrients are washed away during wet season. Often called the hungry soil",
-        "how_to_improve": "add organic matters that can hold and retain water",
-        "suitable_plants": "bulbs and tubers"
+        "How to improve the soil": "add organic matters that can hold and retain water",
+        "Suitable Plants": "bulbs and tubers"
     },
     {
         "name": "silt",
-        "colors": "dark brown, grayish brown",
-        "grain_size": "bigger than clay, smaller than sand",
-        "acidity": "slightly acidic to slightly alkaline",
-        "texture": "very fine and soft",
-        "how_to_test": "smooth and soapy to the touch",
-        "water_and_nutrient_holding_capacity": "lower than clay, higher than sand",
-        "porosity": "good",
-        "other_properties": "easy to compact",
-        "how_to_improve": "add organic matter, cultivation",
-        "suitable_plants": "majority of vegetables"
+        "Colors": "dark brown, grayish brown",
+        "Grain Size": "bigger than clay, smaller than sand",
+        "Acidity": "slightly acidic to slightly alkaline",
+        "Texture": "very fine and soft",
+        "How to verify the type": "smooth and soapy to the touch",
+        "Water and Nutrient Holding Capacity": "lower than clay, higher than sand",
+        "Porosity": "good",
+        "Other properties": "easy to compact",
+        "How to improve the soil": "add organic matter, cultivation",
+        "Suitable Plants": "majority of vegetables"
     },
     {
         "name": "humus",
-        "colors": "dark brown, black",
-        "grain_size": "small to soft large size",
-        "acidity": "acidic",
-        "texture": "soft and easy to compress",
-        "how_to_test":
+        "Colors": "dark brown, black",
+        "Grain Size": "small to soft large size",
+        "Acidity": "acidic",
+        "Texture": "soft and easy to compress",
+        "How to verify the type":
             "when wet peat soil is rolled, you won'nt form a ball. It's spongy to the touch and when squeezed, water drains through your fingers",
-        "water_and_nutrient_holding_capacity": "high",
-        "porosity": "low",
-        "other_properties": "high in water content, high in organic matter",
-        "how_to_improve": "add organic matter, cultivation, add limestone",
-        "suitable_plants": "majority of vegetables"
+        "Water and Nutrient Holding Capacity": "high",
+        "Porosity": "low",
+        "Other properties": "high in water content, high in organic matter",
+        "How to improve the soil": "add organic matter, cultivation, add limestone",
+        "Suitable Plants": "majority of vegetables"
     },
     {
         "name": "gravel",
-        "colors": "dark brown, black",
-        "grain_size": "big chunks",
-        "acidity": "neutral to alkaline",
-        "texture": "rough",
-        "how_to_test": "has evident white chalks in the soil",
-        "water_and_nutrient_holding_capacity": "low",
-        "porosity": "high",
-        "other_properties": "dries out rapidly in hot weather",
-        "how_to_improve":
+        "Colors": "dark brown, black",
+        "Grain Size": "big chunks",
+        "Acidity": "neutral to alkaline",
+        "Texture": "rough",
+        "How to verify the type": "has evident white chalks in the soil",
+        "Water and Nutrient Holding Capacity": "low",
+        "Porosity": "high",
+        "Other properties": "dries out rapidly in hot weather",
+        "How to improve the soil":
             "amend the soil by adding organic matter, such as composted manure or peat, and tillig it into the top eight inches of soil",
-        "suitable_plants": "bulbs and tubers"
+        "Suitable Plants": "bulbs and tubers"
     }
 ];
