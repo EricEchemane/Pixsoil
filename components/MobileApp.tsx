2e531ef9e0df2b88e350712d739d89554504c926
Eric Echemane <eechemane29@gmail.com>
2023-06-02 21:50:48 +0800

    try video

--- components/MobileApp.tsx
+++ components/MobileApp.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 
 export default function MobileApp() {
-    return (
-        <div className='mobile'>
-            <video
-                poster='/mobile_app_poster.png'
-                src="https://res.cloudinary.com/dmwjwtg1g/video/upload/v1659630588/samples/app_demo_a6evsn.mp4"
-                muted
-                autoPlay
-                loop />
-            <img className='frame' src="/android_frame.png" alt="mobile app frame" />
-        </div>
-    );
+  return (
+    <div className='mobile'>
+      <video
+        poster='/mobile_app_poster.png'
+        // src='https://res.cloudinary.com/dmwjwtg1g/video/upload/v1659630588/samples/app_demo_a6evsn.mp4'
+        src='/app_demo.mp4'
+        muted
+        autoPlay
+        loop
+      />
+      <img className='frame' src='/android_frame.png' alt='mobile app frame' />
+    </div>
+  );
 }
